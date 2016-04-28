f725f414247cbb99f3782b7654e9d86163a56629
lsiminski <laurensiminski@gmail.com>
2016-04-28 11:41:55 -0400

    asdasd

--- os.js
+++ os.js
@@ -8,7 +8,7 @@ $.getJSON('oblique.json', function(data) {
 $.each(data, function(data) {
 		  $("#oblique_category p").text(this.category);
 		  $("#oblique_prompt p").text(this.prompt);
-		  $("#oblique_icons p").image(this.icons);
+		  $("#oblique_icons p").Image(this.icons);
 		  /* $("body.random").css("background-image","url(" + this.image + ")"); */
   });
     
