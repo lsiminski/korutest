664d66d11fa6d310a500d911c67886d0a1ea1f97
lsiminski <laurensiminski@gmail.com>
2016-04-28 00:17:39 -0400

    afasdf

--- os.js
+++ os.js
@@ -2,7 +2,7 @@
 $.getJSON('oblique.json', function(data) { 
     
   var randomStrategy = data.promptcards[Math.floor(Math.random()*data.promptcards.length)];
-  /* console.log(randomStrategy.prompt); */
+  console.log(randomStrategy.promptcards);
   /* $("#oblique_promptcards p").text(randomStrategy.prompt); */
 
 
