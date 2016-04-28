ffea53f907f0f7a40d53e66a44c843a6366f111b
lsiminski <laurensiminski@gmail.com>
2016-04-28 14:55:43 -0400

    html in icons js & tinypic urls in json

--- os.js
+++ os.js
@@ -8,9 +8,12 @@ $.getJSON('oblique.json', function(data) {
 $.each(data, function(data) {
 		  $("#oblique_category p").text(this.category);
 		  $("#oblique_prompt p").text(this.prompt);
-		  $("#oblique_icons p").text(this.Image);
+		  $("#oblique_icons p").html("<img src='" + this.url + "'>");
 		  $("body.random").css("background-color");
   });
+    
+   /*  $("#oblique_icons p").text(this.Image); */
+  /*  $("#list_name p").html("<a href='" + this.url + "' target=blank>" + this.name + "</a>"); */
     
 });
 
