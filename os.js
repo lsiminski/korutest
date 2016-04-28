ef10a946dea5db509fb316e10b139e8dbdd524f2
lsiminski <laurensiminski@gmail.com>
2016-04-28 15:01:53 -0400

    click function

--- os.js
+++ os.js
@@ -8,12 +8,12 @@ $.getJSON('oblique.json', function(data) {
 $.each(data, function(data) {
 		  $("#oblique_category p").text(this.category);
 		  $("#oblique_prompt p").text(this.prompt);
-		  $("#oblique_icons p").html("<img src='" + this.icons + "'>");
+		  $("#oblique_icons p").html("<img src='" + this.url + "'>");
 		  $("body.random").css("background-color");
   });
 
 });
 
-$('#oblique_cards').click(function() {
+$('#oblique_prompt').click(function() {
   document.location.reload(true);
 });
