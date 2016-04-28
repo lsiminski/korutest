4c394c7c87c3a4bc0c084e47b3838ca4b6ec2fee
lsiminski <laurensiminski@gmail.com>
2016-04-28 11:22:39 -0400

    zdfzdfv

--- os.js
+++ os.js
@@ -5,16 +5,10 @@ $.getJSON('oblique.json', function(data) {
   console.log(randomCard);
   /* $("#oblique_strategies p").text(randomStrategy.strategy); */
 
-  /*
-$.each(data.strategies, function(data) {
-    $('#fulllist').append('<li class="strategy"><p>' + this["strategy"] + '</p></li>');
-  });
-*/
-
 $.each(data, function(data) {
-		  $("#list_prompt p").text(this.prompt);
-		  $("#list_icons p").text(this.img);
-		  $("#list_category p").text(this.category);
+		  $("#oblique_prompt p").text(this.prompt);
+		  $("#oblique_icons p").text(this.img);
+		  $("#oblique_category p").text(this.category);
 		  $("body.random").css("background-image","url(" + this.image + ")");
 	  } else {
 	  }
