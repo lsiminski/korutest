7c2fad800f6dc27944a220bb1e089c861c6e47da
lsiminski <laurensiminski@gmail.com>
2016-04-28 00:07:08 -0400

    sdfvsfv

--- os.js
+++ os.js
@@ -5,6 +5,17 @@ $.getJSON('oblique.json', function(data) {
   console.log(randomStrategy.prompt);
   $("#oblique_promptcards p").text(randomStrategy.prompt);
 
+
+$.each(data, function(data) {
+	  if ( this.date === moda ) {
+		  $("#oblique_prompt p").text(this.prompt);
+		  $("#oblique_category p").text(this.category);
+		  $("#oblique_icons p").text(this.Image);
+		  $("body.random").css("background-image","url(" + this.image + ")");
+	  } else {
+	  }
+  });
+
   /*
 $.each(data.promptcards, function(data) {
     $('#fulllist').append('<li class="prompt"><p>' + this["prompt"] + '</p></li>');
